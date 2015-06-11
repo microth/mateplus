5b309c1114a9847ea1e7473d407fe53a5702684b
microth <mroth@inf.ed.ac.uk>
2015-06-11 14:32:06 +0100

    Updated JAR file, German parsing script

---
+++ scripts/parse-ger.sh
@@ -0,0 +1,12 @@
+# please download these models and adjust their locations accordingly
+LEMMA_MODEL=models/lemma-ger.model
+PARSER_MODEL=models/parse-ger.model # Bernd's joint tagging/parsing model
+TOKEN_MODEL=models/token-ger.model # German tokenization model for OpenNLP
+
+RERANKER="-reranker"
+TOKENIZE="-tokenize -token $TOKEN_MODEL"
+
+JAVA=/usr/lib/jvm/java-1.8.0/bin/java
+
+#parse $1
+$JAVA -cp "lib/opennlp-maxent-3.0.3.jar:lib/opennlp-tools-1.5.3.jar:lib/liblinear-1.51-with-deps.jar:lib/stanford-corenlp-3.5.0.jar:lib/transition-1.30.jar:lib/whatswrong-0.2.3.jar:mateplus.jar" -Xmx6g se.lth.cs.srl.CompletePipeline eng -lemma $LEMMA_MODEL -parser $PARSER_MODEL -hybrid -srl models/srl-EMNLP14+fs-ger.model $RERANKER $TOKENIZE -test $1
