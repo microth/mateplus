605eafb539948d30420dda7024ff2bdf4bca92a3
microth <mroth@inf.ed.ac.uk>
2015-06-18 15:50:05 +0100

    Changed parsing language for German to German

--- scripts/parse-ger.sh
+++ scripts/parse-ger.sh
@@ -9,4 +9,4 @@ TOKENIZE="-tokenize -token $TOKEN_MODEL"
 JAVA=/usr/lib/jvm/java-1.8.0/bin/java
 
 #parse $1
-$JAVA -cp "lib/opennlp-maxent-3.0.3.jar:lib/opennlp-tools-1.5.3.jar:lib/liblinear-1.51-with-deps.jar:lib/stanford-corenlp-3.5.0.jar:lib/transition-1.30.jar:lib/whatswrong-0.2.3.jar:mateplus.jar" -Xmx6g se.lth.cs.srl.CompletePipeline eng -lemma $LEMMA_MODEL -parser $PARSER_MODEL -hybrid -srl models/srl-EMNLP14+fs-ger.model $RERANKER $TOKENIZE -test $1
+$JAVA -cp "lib/opennlp-maxent-3.0.3.jar:lib/opennlp-tools-1.5.3.jar:lib/liblinear-1.51-with-deps.jar:lib/stanford-corenlp-3.5.0.jar:lib/transition-1.30.jar:lib/whatswrong-0.2.3.jar:mateplus.jar" -Xmx6g se.lth.cs.srl.CompletePipeline ger -lemma $LEMMA_MODEL -parser $PARSER_MODEL -hybrid -srl models/srl-EMNLP14+fs-ger.model $RERANKER $TOKENIZE -test $1
