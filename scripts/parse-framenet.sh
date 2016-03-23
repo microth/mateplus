94122429ce235b9c1bb5a0417542bccceccc9cb1
microth <mroth@inf.ed.ac.uk>
2016-03-23 09:44:00 +0000

    * Added "-globalFeats" option to FrameNet example script. This activates global features in the reranker and is essential to reproduce state-of-the-art results. * Removed "-lemma" parameter from German example script with external parser, as the external tools already do lemmatization.

--- scripts/parse-framenet.sh
+++ scripts/parse-framenet.sh
@@ -7,4 +7,4 @@ FRAMENETDIR=/disk/scratch/framenet/fndata-1.5/
 # (please also modify processtmp.sh in glove dir accordingly!)
 GLOVEDIR=/disk/scratch/glove/ 
 
-$JAVA -Xmx8g -cp lib/*:mateplus.jar se.lth.cs.srl.CompletePipeline eng -lemma models/lemma-eng.model -parser models/parse-eng.model -tagger models/tagger-eng.model -srl models/srl-TACL15-eng.model -glove $GLOVEDIR -reranker -semafor 127.0.0.1:8043 -mst 127.0.0.1:12345 -framenet $FRAMENETDIR -test $1
+$JAVA -Xmx8g -cp lib/*:mateplus.jar se.lth.cs.srl.CompletePipeline eng -lemma models/lemma-eng.model -parser models/parse-eng.model -tagger models/tagger-eng.model -srl models/srl-TACL15-eng.model -glove $GLOVEDIR -reranker -globalFeats -semafor 127.0.0.1:8043 -mst 127.0.0.1:12345 -framenet $FRAMENETDIR -test $1
